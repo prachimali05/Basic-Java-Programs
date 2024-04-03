ec04f618ed8f1a4235096817cf8b3b6283e5fe3b
Prachi Mali <146082526+prachimali05@users.noreply.github.com>
2024-04-03 15:31:22 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,14 @@
+public class Sum
+{
+public static void main(String args[])
+{
+int i;
+int sum = 0;
+for(i=0;i<=10;i++)
+{
+sum = sum+i;
+}
+System.out.println("Sum of 10 number is" +sum);
+
+}
+}
